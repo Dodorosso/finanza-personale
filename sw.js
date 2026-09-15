489f8814274ea874d5cdd120efcc55a7972a3c8d
Dodorosso <dod007@gmail.com>
2026-09-15 19:25:30 +0200

    Refresh PWA cache for PiggyBanky icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'finanza-spa-v1';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon.svg'];
+const CACHE = 'piggybanky-v2';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/piggybaky.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
